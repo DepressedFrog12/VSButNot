390e3573f8dce76560e75b64e4530d66f96208a7
DepressedFrog12 <pranav.paati@gmail.com>
2024-11-15 10:00:56 -0500

    Changed to tilemap and changed sprites

--- player.gd
+++ player.gd
@@ -5,6 +5,7 @@ extends CharacterBody2D
 @export var move_speed = 200
 
 var dead = false
+var looking = 90
 
 func _process(delta):
 	if Input.is_action_just_pressed("exit"):
@@ -13,8 +14,9 @@ func _process(delta):
 		restart()
 	if dead:
 		return
-		
-	global_rotation = global_position.direction_to(get_global_mouse_position()).angle() + PI/2.0
+	
+	if (velocity.x == -1):
+		return
 	
 func _physics_process(delta: float):
 	if dead:
@@ -22,6 +24,7 @@ func _physics_process(delta: float):
 	var move_dir = Input.get_vector("move_left","move_right","move_up","move_down")
 	velocity = move_dir * move_speed
 	move_and_slide()
+	
 	
 func kill():
 	if dead:
